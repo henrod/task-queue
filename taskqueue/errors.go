43a011a9b142ed9cf11a6209884148c560170b4a
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-06-24 14:40:01 -0300

    Add linter

--- taskqueue/errors.go
+++ taskqueue/errors.go
@@ -1,6 +1,7 @@
 package taskqueue
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
@@ -11,5 +12,11 @@ func ErrTaskLost(taskID uuid.UUID, err error) error {
 		return nil
 	}
 
-	return fmt.Errorf("task was permanentely lost without being executed: %s, %w", taskID, err)
+	return fmt.Errorf("task was permanently lost without being executed: %s, %w", taskID, err)
 }
+
+var (
+	ErrMaxTaskReties   = errors.New("task reached max retries")
+	ErrInvalidTaskType = errors.New("task not of type string")
+	ErrNoTaskToConsume = errors.New("no task to consume")
+)
