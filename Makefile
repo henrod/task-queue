11e7ccfa1f67b379b6e14adb2bb8fbe8f5d78271
Thiago Machado <thiago.machado@wildlifestudios.com>
2022-06-24 21:43:48 -0300

    WIP Implementing integration tests

--- Makefile
+++ Makefile
@@ -5,6 +5,10 @@ tests: unit-tests
 unit-tests:
 	go test github.com/Henrod/task-queue/taskqueue -v -tags=unit
 
+.PHONY: integration-tests
+integration-tests:
+	go test -timeout 5s -tags unit github.com/Henrod/task-queue/test
+
 .PHONY: setup
 setup:
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
---
+++ test/integration_test.go
@@ -0,0 +1,61 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Henrod/task-queue/taskqueue"
+	"github.com/google/uuid"
+)
+
+func TestTaskQueueMainFlow(t *testing.T) {
+	options := &taskqueue.Options{
+		QueueKey:         "integration-test",
+		Namespace:        "simple",
+		StorageAddress:   "localhost:6379",
+		WorkerID:         "worker1",
+		MaxRetries:       0,
+		OperationTimeout: time.Minute,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	taskQueue, err := taskqueue.NewTaskQueue(ctx, taskqueue.NewDefaultRedis(options), options)
+	if err != nil {
+		panic(err)
+	}
+
+	StartProducer(ctx, taskQueue)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(1)
+	go StartConsumer(ctx, cancel, taskQueue, waitGroup)
+	waitGroup.Wait()
+}
+
+type Payload struct {
+	Body string
+}
+
+func StartProducer(ctx context.Context, taskQueue *taskqueue.TaskQueue) error {
+	id := 1234
+	_, err := taskQueue.ProduceAt(ctx, &Payload{Body: fmt.Sprintf("%d", id)}, time.Now())
+	if err != nil {
+		return err	
+	}
+	return nil
+}
+
+func StartConsumer(ctx context.Context, cancel context.CancelFunc, taskQueue *taskqueue.TaskQueue, waitGroup sync.WaitGroup) error {
+	taskQueue.Consume(
+		ctx,
+		func(ctx context.Context, taskID uuid.UUID, payload interface{}) error {
+			defer waitGroup.Done()
+			cancel()
+			ctx.Done()
+			return nil
+		},
+	)
+	return nil
+}
